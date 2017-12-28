ed107a1b4750bc0e1d399e5bb247caff9758e671
Enric Bisbe Gil <enricu@gmail.com>
2017-12-28 17:21:53 +0100

    Add SendDocuments method

---
+++ src/SendDocuments.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Holded;
+
+use Holded\Abstracts\BaseApi;
+
+class SendDocuments extends BaseApi
+{
+    public function __construct(Caller $call)
+    {
+        $this->call = $call;
+    }
+
+    public function send($doctype, $docid, $fields)
+    {
+        return $this->execute(__METHOD__, $fields, "$doctype/$docid");
+    }
+}
